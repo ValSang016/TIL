2108fe8506aae9bb9c30694137c6d889da98776a
ValSang016 <tkdgjs1315@naver.com>
2023-10-08 20:50:16 +0900

    DB and PS 23/10/08

--- Lecture2-2/Database/database_practice.sql
+++ Lecture2-2/Database/database_practice.sql
@@ -1,5 +1,6 @@
 show databases;
 use university;
+select * from student;
 select * from instructor;
 select * from teaches;
 select * from instructor, teaches where instructor.ID = teaches = ID;
@@ -46,4 +47,28 @@ select * from instructor where dept_name = 'Finance';
 create view instructor_FC as select * from instructor where dept_name = 'Finance';
 insert into instructor_FC values ('25565', 'Brown', 'Biology', '100000');
 select * from instructor_FC; -- 위의 insert에 오류른 없으나 반영되지 않음 
+select * from instructor where dept_name = 'Biology'; -- 하지만 original DB에는 반영됨!!
 
+select curdate();
+
+select * from teaches;
+select * from instructor natural join teaches;
+select * from instructor;
+
+desc instructor;
+select * from grade_points;
+select * from takes;
+select * from (takes natural join course) NATURAL LEFT JOIN grade_points;
+select * from student natural join (select ID, sum(credits) from takes natural join course group by ID) as K;
+select takes.ID, avg(points) as GPA from takes join grade_points using (grade) natural join student  group by ID;
+
+select ID, credit_points / if(credit_sum = 0, null, credit_sum)
+from  (
+select ID, sum(if(grade is null, 0, credits)) as credit_sum,
+		sum(if(grade is null, 0, credits * points)) as credit_points 
+        from takes natural join course
+        natural left join grade_points 
+        group by ID
+union
+select ID, null, null from student
+where ID not in (select ID from takes)) as derived_table;
---
+++ Lecture2-2/Problem_solving/Ecercise12.2.3.c
@@ -0,0 +1,70 @@
+/*
+* File: Exercise12.2.3.C
+*
+* Author : Sangheon Lee(tkdgjs1315@gachon.ac.kr)
+* Date : Oct.08 2023
+* Course : Problem solving
+* Summary : This file contain code that practice in the class.
+*
+*/
+
+#include <stdio.h>
+
+#define number 6 // number of employees
+
+/*
+*
+* struct ID(int), last_name[20](char), pay_rate(float), hours_worked(float)
+*
+* Summary of this function : 
+*   This function calculates multipulation, division, remainder, plus of two values.
+*
+* Parameter : int val1, int val2, int *re1, int *re2, int *re3, int *re4 / By pointer
+* 
+*/
+struct 
+{
+  int ID;
+  char last_name[20];
+  float pay_rate;
+  float hours_worked;
+}employees[number];
+
+
+/*
+*
+* void main()
+*
+* Summary of this function : 
+*   This function receives imformations of employees(ID, name, payrate, hours worked)
+*   and caculate total pay of each employees and print a payroll report.
+* 
+*/
+void main(){
+  float total_pay[number]; // Total payroll of each employees
+  float total_payroll = 0; // Total payroll of all employees
+
+  //Receive informations of employees and save the total pay of each employees
+  for (int i = 0; i < number; i++)
+  {
+    printf("Enter the ID, last_name, payrate, hours worked of %dth employee\n", i+1);
+    scanf("%d %s %f %f", &employees[i].ID, employees[i].last_name, &employees[i].pay_rate, &employees[i].hours_worked);
+    total_pay[i] = employees[i].pay_rate * employees[i].hours_worked; 
+  }
+
+  // Sum the total payroll of all employees
+  for (int i = 0; i < number; i++)
+  {
+    total_payroll += total_pay[i];
+  }
+  
+  // Print the payroll report
+  printf("|   ID   |    NAME    |  Total pay  |\n");
+  for (int i = 0; i < number; i++)
+  {
+    printf("|  %d  | %7s    |    %.2f   |\n", employees[i].ID, employees[i].last_name, total_pay[i]);
+  }
+  printf("Total payroll of all employees : %.2f", total_payroll);
+  
+  
+}
